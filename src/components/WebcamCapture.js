f7f1628354156253a123f5e8608b741cb7de1e68
pairofAces <karanchauhan16@gmail.com>
2021-01-30 17:31:20 -0500

    working on WebcamCapture.js

--- src/components/WebcamCapture.js
+++ src/components/WebcamCapture.js
@@ -1,6 +1,29 @@
+import React, { useRef } from 'react';
+import Webcam from 'react-webcam';
+
 // create video constraints to set the size of the camera video
 const videoConstraints = {
     width: 250,
     height: 400,
     facingMode: "user",
 }
+
+
+function WebcamCapture() {
+    // create a reference point for the webcam
+    const webcamRef = useRef(null);
+
+    return (
+        <div className="webcamCapture">
+            <Webcam 
+                audio={false}
+                height={videoConstraints.height}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                width={videoConstraints.width}
+            />
+        </div>
+    )
+}
+
+export default WebcamCapture;
