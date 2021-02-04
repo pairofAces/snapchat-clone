f9a11bf6812cddfe60e3955e2a788762d1799952
pairofAces <karanchauhan16@gmail.com>
2021-02-03 21:16:05 -0500

    included selectCameraImage from cameraSlice to Preview

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectCameraImage } from '../../features/cameraSlice';
 import './Preview.css';
 
 function Preview() {
+    // pull the image required in the preview div
+    // from the redux-data-layer
+    const cameraImage = useSelector(selectCameraImage);
+
     return (
         <div className="preview">
-            
+            {/* preview of the image goes here */}
+            <img src={selectCameraImage} alt=""/>
         </div>
     )
 }
