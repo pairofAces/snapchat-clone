44853566647f2d217bc53274c2ad1d69540bf853
pairofAces <karanchauhan16@gmail.com>
2021-02-04 21:59:59 -0500

    imported material-UI icons and inserted into preview__toolbarRight div

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -4,6 +4,13 @@ import { useHistory } from 'react-router-dom';
 import { resetCameraImage, selectCameraImage } from '../../features/cameraSlice';
 import './Preview.css';
 import CloseIcon from '@material-ui/icons/Close';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import CreateIcon from '@material-ui/icons/Create';
+import NoteIcon from '@material-ui/icons/Note';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import CropIcon from '@material-ui/icons/Crop';
+import TimerIcon from '@material-ui/icons/Timer';
 
 function Preview() {
     // pull the image required in the preview div
@@ -28,7 +35,13 @@ function Preview() {
             {/* <h1>This is the preview of the image</h1> */}
             <CloseIcon onClick={closePreview} className="preview__close"/>
             <div className="preview__toolbarRight">
-
+                <TextFieldsIcon />
+                <CreateIcon />
+                <NoteIcon />
+                <MusicNoteIcon />
+                <AttachFileIcon />
+                <CropIcon />
+                <TimerIcon />
             </div>
             <img src={cameraImage} alt=""/>
         </div>
