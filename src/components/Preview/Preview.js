3233fb4371ec5e7514d9bf200502553c8019be82
pairofAces <karanchauhan16@gmail.com>
2021-02-04 21:45:36 -0500

    imported close icon into Preview.js and working on closePreview onClick

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { selectCameraImage } from '../../features/cameraSlice';
 import './Preview.css';
+import CloseIcon from '@material-ui/icons/Close';
 
 function Preview() {
     // pull the image required in the preview div
@@ -16,10 +17,15 @@ function Preview() {
         }
      }, [])
 
+     const closePreview = () => {
+         
+     }
+
     return (
         <div className="preview">
             {/* preview of the image goes here */}
-            <h1>This is the preview of the image</h1>
+            {/* <h1>This is the preview of the image</h1> */}
+            <CloseIcon onClick={closePreview} className="preview__close"/>
             <img src={cameraImage} alt=""/>
         </div>
     )
