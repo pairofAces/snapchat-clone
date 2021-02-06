84122dad8f41a6c9d1202238aa49e2b1e8208cd7
pairofAces <karanchauhan16@gmail.com>
2021-02-05 20:44:16 -0500

    imported and displayed basic avatar and search icon in /chats

--- src/components/Chats/Chats.js
+++ src/components/Chats/Chats.js
@@ -1,10 +1,17 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Chats.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Chats() {
     return (
         <div className="chats">
-            
+            <div className="chats__header">
+                <Avatar className="chats__avatar"/>
+                <div className="chats__search">
+                    <SearchIcon />
+                </div>
+            </div>
         </div>
     )
 }
