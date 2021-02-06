1cf562663cc9350536e096ba978be2640bee2bf0
pairofAces <karanchauhan16@gmail.com>
2021-02-05 20:48:12 -0500

    imported and implemented the ChatBubbleIcon from material-ui to Chats.js

--- src/components/Chats/Chats.js
+++ src/components/Chats/Chats.js
@@ -2,6 +2,7 @@ import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Chats.css';
 import SearchIcon from '@material-ui/icons/Search';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 
 function Chats() {
     return (
@@ -10,7 +11,9 @@ function Chats() {
                 <Avatar className="chats__avatar"/>
                 <div className="chats__search">
                     <SearchIcon />
+                    <input placeholder="Friends" type="text"/> 
                 </div>
+                <ChatBubbleIcon className="chats__chatIcon"/>
             </div>
         </div>
     )
