3b90975542127fd2ffb26e01d62e746ff465c64d
pairofAces <karanchauhan16@gmail.com>
2021-02-07 15:05:35 -0500

    included a StopRoundedIcon for each individual Chat component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,15 +1,18 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Chat.css';
+import StopRoundedIcon from '@material-ui/icons/StopRounded';
 
 function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
     return (
         <div className="chat">
-            <Avatar src={profilePic}/>
+            <Avatar className="chat__avatar" src={profilePic}/>
             <div className="chat__info">
                 <h4>{username}</h4>
                 <p>Tap to view - {new Date(timestamp?.toDate()).toUTCString()}</p>
             </div>
+
+            {!read && <StopRoundedIcon className="chat__readIcon"/>}
         </div>
     )
 }
