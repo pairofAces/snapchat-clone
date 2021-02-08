04fd7b055d2e4e352d4eff00dccac481735c9e9e
pairofAces <karanchauhan16@gmail.com>
2021-02-07 19:43:50 -0500

    working on the ChatView component

---
+++ src/components/ChatView/ChatView.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { selectSelectedImage } from '../../features/appSlice';
+import './ChatView.css';
+
+function ChatView() {
+    const selectedImage = useSelector(selectSelectedImage);
+    const history = useHistory();
+
+    console.log(selectedImage)
+    useEffect(() => {
+        if (!selectedImage) {
+            exit();
+        }
+    }, [selectedImage])
+
+    const exit = () => {
+        history.replace('/chats')
+    };
+
+    return (
+        <div className="chatView">
+            <img src={selectedImage} onClick={exit} alt=""/>
+        </div>
+    )
+}
+
+export default ChatView
