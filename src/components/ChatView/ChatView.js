4fd07ef0acfaa1089f9efd804f32d8450cdae07f
pairofAces <karanchauhan16@gmail.com>
2021-02-08 22:39:02 -0500

    remainingTime functionality is working

--- src/components/ChatView/ChatView.js
+++ src/components/ChatView/ChatView.js
@@ -30,14 +30,15 @@ function ChatView() {
                 size={50}
                 colors={[
                     ["#004777", 0.33],
-                    ["F78801", 0.33],
-                    ["A30000", 0.33],
+                    ["#F78801", 0.33],
+                    ["#A30000", 0.33],
                 ]}
             >
                 {({remainingTime}) => {
                     if (remainingTime === 0) {
                         exit();
                     }
+                    return remainingTime;
                 }}
             </CountdownCircleTimer>
         </div>
