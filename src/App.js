2ad08518e74b4a6c7378454840052e40b77bdd77
pairofAces <karanchauhan16@gmail.com>
2021-02-09 22:04:19 -0500

    aspiraitonal code for the login component in App.js -> working on login.js/css

--- src/App.js
+++ src/App.js
@@ -9,36 +9,41 @@ import {
 } from "react-router-dom";
 import Chats from './components/Chats/Chats';
 import ChatView from './components/ChatView/ChatView';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from './features/appSlice';
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   return (
     <div className="app">
       {/* <h1> Let's build this snapchat clone!</h1> */}
       <Router>
-        <div className="app__body">
-        <Switch>
-          <Route exact path="/about">
-            <h1>This will be the about page</h1>
-            {/* <About /> */}
-          </Route>
-          <Route exact path="/">
-            <WebcamCapture />
-          </Route>
-          <Route exact path="/preview">
-            <Preview />
-          </Route>
-          <Route exact path="/chats">
-            <Chats />
-          </Route>
-          <Route exact path="/chats/view">
-            <ChatView />
-          </Route>
-        </Switch>
-      </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="app__body">
+          <Switch>
+            <Route exact path="/about">
+              <h1>This will be the about page</h1>
+              {/* <About /> */}
+            </Route>
+            <Route exact path="/">
+              <WebcamCapture />
+            </Route>
+            <Route exact path="/preview">
+              <Preview />
+            </Route>
+            <Route exact path="/chats">
+              <Chats />
+            </Route>
+            <Route exact path="/chats/view">
+              <ChatView />
+            </Route>
+          </Switch>
+        </div>
+        )}
     </Router>
       {/* react-webcam to use camera capabilities */}
     </div>
