8ae1caa95f24abec0f493d3c58406cd57c3bde6f
pairofAces <karanchauhan16@gmail.com>
2021-02-09 21:46:13 -0500

    created div for the countdown cirlce timer

--- src/components/ChatView/ChatView.js
+++ src/components/ChatView/ChatView.js
@@ -23,24 +23,26 @@ function ChatView() {
     return (
         <div className="chatView">
             <img src={selectedImage} onClick={exit} alt=""/>
-            <CountdownCircleTimer
-                isPlaying
-                duration={10}
-                strokeWidth
-                size={50}
-                colors={[
-                    ["#004777", 0.33],
-                    ["#F78801", 0.33],
-                    ["#A30000", 0.33],
-                ]}
-            >
-                {({remainingTime}) => {
-                    if (remainingTime === 0) {
-                        exit();
-                    }
-                    return remainingTime;
-                }}
-            </CountdownCircleTimer>
+            <div className="chatView__timer">
+                <CountdownCircleTimer
+                    isPlaying
+                    duration={10}
+                    strokeWidth
+                    size={50}
+                    colors={[
+                        ["#004777", 0.33],
+                        ["#F78801", 0.33],
+                        ["#A30000", 0.33],
+                    ]}
+                >
+                    {({remainingTime}) => {
+                        if (remainingTime === 0) {
+                            exit();
+                        }
+                        return remainingTime;
+                    }}
+                </CountdownCircleTimer>
+            </div>
         </div>
     )
 }
