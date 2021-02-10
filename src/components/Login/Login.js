3e3a4424d5eeab6b9a2d4fd8257a536edee77cde
pairofAces <karanchauhan16@gmail.com>
2021-02-10 15:06:32 -0500

    login component is given flex/column display

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './Login.css';
-import img from '../../ghost.jpg'
+// import img from '../../ghost.jpg'
 import { Button } from '@material-ui/core';
 
 function Login() {
     const signIn = () => {
 
     };
-    
+
     return (
         <div className="login">
             <div className="login__container">
-                <img src={img} alt=""/>
+                <img src="https://cdn3.iconfinder.com/data/icons/popular-social-media-glyph/48/Sed-15-512.png" alt=""/>
                 <Button variant="outline" onClick={signIn}>Sign In</Button>
             </div>
         </div>
