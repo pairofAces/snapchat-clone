9dd1f8ea29619fa42bb19a544d044c66fe419929
pairofAces <karanchauhan16@gmail.com>
2021-02-10 14:58:34 -0500

    implemented a signIn button working on signIn function and login.css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Login.css';
 import img from '../../ghost.jpg'
+import { Button } from '@material-ui/core';
 
 function Login() {
+    const signIn = () => {
+
+    };
+    
     return (
         <div className="login">
             <div className="login__container">
                 <img src={img} alt=""/>
-                
+                <Button variant="outline" onClick={signIn}>Sign In</Button>
             </div>
         </div>
     )
