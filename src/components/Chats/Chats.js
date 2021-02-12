1e368d8283c06ab5d0a367938f6ab0bb7a62cf88
pairofAces <karanchauhan16@gmail.com>
2021-02-11 21:45:57 -0500

    inserted a RadioButtonUncheckedIcon in Chats.js-View

--- src/components/Chats/Chats.js
+++ src/components/Chats/Chats.js
@@ -3,10 +3,11 @@ import React, { useEffect, useState } from 'react';
 import './Chats.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
-import { db } from '../../firebase';
+import { auth, db } from '../../firebase';
 import Chat from '../Chat/Chat';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../features/appSlice';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 
 function Chats() {
      const [posts, setPosts] = useState([]);
@@ -24,11 +25,15 @@ function Chats() {
                 )
             );
     }, []);
+
+    const takeSnap = () => {
+
+    };
     
     return(
         <div className='chats'>
             <div className='chats__header'>
-                <Avatar src={user.profilePic} className='chats__avatar' />
+                <Avatar src={user.profilePic} onClick={() => auth.signOut()} className='chats__avatar' />
                 <div className='chats__search'>
                     <SearchIcon />
                     <input placeholder='Friends' type='text' />
@@ -53,6 +58,11 @@ function Chats() {
                     />
                 ))}
             </div>
+            <RadioButtonUncheckedIcon 
+                className="chats__takePicIcon"
+                onClick={takeSnap}
+                fontSize='large'
+            />
         </div>
     )
 }
